ebc54d436336ef4aee07c26beff19acdc4055b73
danielAwale <danielmadhukarawale@gmail.com>
2022-09-14 10:24:17 -0700

    contain test for daylistItem and will create a function to pass all 4 test

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,6 +3,18 @@ import 'components/DayListItem.scss';
 import classNames from 'classnames';
 
 export default function DayListItem(props) {
+  function formatSpots(spots) {
+    if (spots === 1) {
+      return `${spots} spot remaining`;
+
+    }
+    if (spots === 0) {
+      return `no spots remaining`;
+    }
+    return `${spots} spots remaining`;
+  }
+
+
   let dayClass = classNames('day-list__item', {
     'day-list__item--selected': props.selected,
     'day-list__item--full': !props.spots
@@ -10,7 +22,7 @@ export default function DayListItem(props) {
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className='text--regular'>{props.name}</h2>
-      <h3 className='text--light'> {props.spots} spots remaining</h3>
+      <h3 className='text--light'> {formatSpots(props.spots)}</h3>
     </li>
   )
 }
