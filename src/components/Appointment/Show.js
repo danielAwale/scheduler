47d1415dbee80de6bcd5e86cf9c2e179fe4bf122
danielAwale <danielmadhukarawale@gmail.com>
2022-09-18 09:39:18 -0700

    contain some major changes still the out put is not as expected

--- src/components/Appointment/Show.js
+++ src/components/Appointment/Show.js
@@ -1,16 +1,14 @@
 import React from 'react'
 
 export default function Show(props) {
-  const interviewerName = props.interviewers.map((interviewer) => {
-    return interviewer.name;
-  })
+  const interviewerName = props.interviewer.map(interviewerObj => interviewerObj.interviewer);
   return (
     <main className="appointment__card appointment__card--show">
       <section className="appointment__card-left">
         <h2 className="text--regular">{props.student}</h2>
         <section className="interviewer">
           <h4 className="text--light">Interviewer</h4>
-          <h3 className="text--regular">{interviewerName[0]}</h3>
+          <h3 className="text--regular">{interviewerName}</h3>
         </section>
       </section>
       <section className="appointment__card-right">
--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -10,7 +10,7 @@ export default function InterviewList(props) {
         name={interviewer.name}
         avatar={interviewer.avatar}
         selected={interviewer.id === props.value}
-        setInterviewer={() => props.onChange(interviewer.id)}
+        setInterviewer={(event) => props.onChange(interviewer.id)}
       />
     )
   })
