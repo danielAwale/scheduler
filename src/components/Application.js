1e69232158b7f15058f8a7a7064efde499d79463
danielAwale <danielmadhukarawale@gmail.com>
2022-09-20 08:30:26 -0700

    will contain interviewers object in our state

--- src/components/Application.js
+++ src/components/Application.js
@@ -12,7 +12,8 @@ export default function Application() {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   })
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
