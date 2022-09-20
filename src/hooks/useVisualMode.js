d7f2a9277239a0da8a5a7fdf7ae15c56f6d594ee
danielAwale <danielmadhukarawale@gmail.com>
2022-09-20 15:28:28 -0700

    contain the logic in transition and back (push and pop)

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,12 +2,25 @@ import { useState } from "react"
 
 export default function useVisualMode(initialValue) {
   const [mode, setMode] = useState(initialValue);
+  const [history, setHistory] = useState([initialValue]);
 
   function transition(transitionMode) {
     setMode(transitionMode);
+    history.push(transitionMode);
+    setHistory([...history]);
+
   }
 
-  return { mode, transition }
+  function back() {
+    if (history.length > 1) {
+      history.pop();
+    }
+    setMode(history[history.length - 1]);
+    setHistory([...history]);
+
+  }
+
+  return { mode, transition, back }
 
 
 }
